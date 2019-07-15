739bce0879ea859ba866666bc131234a7619fe26
OtaGran <ujt2h8@gmail.com>
2019-07-15 14:05:41 +0800

    feat : should_print_the_real_purchase_when_call_process

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,8 +1,10 @@
 package cashregister;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
@@ -11,8 +13,21 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        Item[] items = {new Item("pen",1.5),new Item("water",0.6)};
+        Purchase purchase = new Purchase(items);
+
         //when
+
         //then
+
+        Throwable exception  = Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+            cashRegister.process(purchase);;
+        });
+        assertEquals("Not Implemented",exception.getMessage());
+
+
     }
 
     @Test
